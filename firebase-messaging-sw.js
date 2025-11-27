69770c310ee1df6b7f46de01df3daaf2acaa0b3c
Ranset <ransetfleites0@gmail.com>
2025-11-27 16:58:25 -0500

    Refactor Firebase initialization in service worker to include all configuration parameters

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,8 +2,12 @@ importScripts('https://www.gstatic.com/firebasejs/11.6.1/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/11.6.1/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCnY3hILp6EJ6XUFOzTA7dsu7InFYaE8dA", // Opcional en el SW, pero recomendado dejar vacío o config básica
-  messagingSenderId: "301486367280"
+    apiKey: "AIzaSyCnY3hILp6EJ6XUFOzTA7dsu7InFYaE8dA",
+    authDomain: "testpush-645b8.firebaseapp.com",
+    projectId: "testpush-645b8",
+    storageBucket: "testpush-645b8.firebasestorage.app",
+    messagingSenderId: "301486367280",
+    appId: "1:301486367280:web:e9c03b8349ecd8579c83f5"
 });
 
 const messaging = firebase.messaging();
