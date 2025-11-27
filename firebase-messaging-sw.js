cee5093844a1e79382f566bddff7bb7111581120
Ranset <ransetfleites0@gmail.com>
2025-11-27 18:02:03 -0500

    Enhance Firebase messaging service worker with communication channel setup

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,8 @@
 importScripts('https://www.gstatic.com/firebasejs/11.6.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/11.6.1/firebase-messaging-compat.js');
 
+// Configuración básica (puedes dejar los valores vacíos si solo recibes, 
+// pero se recomienda poner tu senderId)
 firebase.initializeApp({
     apiKey: "AIzaSyCnY3hILp6EJ6XUFOzTA7dsu7InFYaE8dA",
     authDomain: "testpush-645b8.firebaseapp.com",
@@ -12,6 +14,14 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
+// CREAR EL CANAL DE COMUNICACIÓN
+// Esto permite hablar con la pestaña abierta del navegador
+const channel = new BroadcastChannel('firebase_messages');
+
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Mensaje recibido en segundo plano ', payload);
+  
+  // IMPORTANTE: Reenviar el mensaje a la página web abierta
+  // para que pueda mostrar la notificación visual (Toast)
+  channel.postMessage(payload);
 });
