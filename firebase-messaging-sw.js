4a0a2de456c9671e52f2932f68dc8930ffbd917e
Ranset <ransetfleites0@gmail.com>
2026-06-02 17:25:12 -0500

    Update Firebase initialization with API key and messaging sender ID

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,8 +2,8 @@ importScripts('https://www.gstatic.com/firebasejs/11.6.1/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/11.6.1/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "TU_API_KEY_AQUI", 
-  messagingSenderId: "TU_SENDER_ID_AQUI"
+  apiKey: "AIzaSyCnY3hILp6EJ6XUFOzTA7dsu7InFYaE8dA", 
+  messagingSenderId: "301486367280"
 });
 
 const messaging = firebase.messaging();
