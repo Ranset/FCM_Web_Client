cefd5a400f30372629ae49e55a6c5e13ca2c720c
Ranset <ransetfleites0@gmail.com>
2026-06-02 17:08:18 -0500

    Remove configuration parameters in service worker for security

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,15 +1,13 @@
 importScripts('https://www.gstatic.com/firebasejs/11.6.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/11.6.1/firebase-messaging-compat.js');
 
-// Configuración básica (puedes dejar los valores vacíos si solo recibes, 
-// pero se recomienda poner tu senderId)
 firebase.initializeApp({
-    apiKey: "AIzaSyCnY3hILp6EJ6XUFOzTA7dsu7InFYaE8dA",
-    authDomain: "testpush-645b8.firebaseapp.com",
-    projectId: "testpush-645b8",
-    storageBucket: "testpush-645b8.firebasestorage.app",
-    messagingSenderId: "301486367280",
-    appId: "1:301486367280:web:e9c03b8349ecd8579c83f5"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 });
 
 const messaging = firebase.messaging();
